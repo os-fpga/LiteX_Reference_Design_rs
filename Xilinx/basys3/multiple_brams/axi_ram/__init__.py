3384a8a1105a3ea3313a2b8308a9d7bef3179626
bilalahmed-RS <bilal.ahmed@rapidsilicon.com>
2022-04-13 10:09:37 +0500

    Multiple axi_rams reference design added

---
+++ Xilinx/basys3/multiple_brams/axi_ram/__init__.py
@@ -0,0 +1,2 @@
+from litex.soc.cores.ip.axi_ram.core_ram import axi_ram
+from litex.soc.cores.ip.axi_ram.core_ram import axi_ram_1
---
+++ Xilinx/basys3/multiple_brams/demo/crt0.d
@@ -0,0 +1,2 @@
+crt0.o: \
+ /home/users/bilal.ahmed/litex_dev/Litex_dev/litex/soc/cores/cpu/vexriscv/crt0.S
